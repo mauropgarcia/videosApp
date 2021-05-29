4217236d64bb176475147e81576ed6090d043fa5
Mauro Garcia <mauro.avare@gmail.com>
2021-05-29 11:15:54 -0300

    adicionamos array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/iFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,49 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  titulo='Videos APP';
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Falcão e o Soldado Invernal (2021)',
+      lancamento: '19/03/2021',
+      duracao: '2h15m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8Hi3GI4q1oR6EImrDWrAQFn8Ha.jpg',
+      generos: ['Ação', 'Drama', 'Guerra']
+    },
+    {
+      nome: 'Jurassic World: Acampamento Jurássico (2020)',
+      lancamento: '05/10/2020',
+      duracao: '1h24m',
+      classificacao: 78,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/kClnDGBEXkiiXK5IZ1BY0VgoPhK.jpg',
+      generos: ['Kids', 'Animação', 'Aventura']
+    },
+    {
+      nome: 'Lobo Guerreiro 2 (2017)',
+      lancamento: '01/07/2017',
+      duracao: '2h4m',
+      classificacao: 66,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/nJ8V8eOLWEKnZjvRZkyI8BRMKDf.jpg',
+      generos: ['Guerra', 'Ação', 'Drama']
+    },
+    {
+      nome: 'O Conto da Aia (2018)',
+      lancamento: '30/04/2018',
+      duracao: '0h50m',
+      classificacao: 82,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/2rK53k6Lg6IDqHM7xp8OTzzzpz7.jpg',
+      generos: ['Fantasia', 'Drama']
+    },
+    {
+      nome: 'A Sentilnela (2021)',
+      lancamento: '10/03/2021',
+      duracao: '1h20m',
+      classificacao: 60,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/56Z9eh4k9pzR6Yhy201nq2Qoa0n.jpg',
+      generos: ['Thriller', 'Drama', 'Ação']
+    }
+  ];
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
   async exibirAletaFavorito() {
